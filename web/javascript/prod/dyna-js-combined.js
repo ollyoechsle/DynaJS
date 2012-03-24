78e11976593524e6d6b7b2a849903001ca10b640
Olly Oechsle <owksley@gmail.com>
2012-03-24 07:59:24 +0000

    Better map view rendering code

--- web/javascript/prod/dyna-js-combined.js
+++ web/javascript/prod/dyna-js-combined.js
@@ -280,35 +280,53 @@ window.Dyna = {
      * Constructor
      */
     function MapView(jContainer, map) {
-
         log("Creating mapview for  " + map.name);
         this.jContainer = jContainer;
         this.map = map;
-
+        this.initialise();
     }
 
     MapView.prototype.map = null;
     MapView.prototype.jContainer = null;
+    MapView.prototype.tileSize = 30;
+    MapView.prototype.tileTemplate = null;
+
+    MapView.prototype.initialise = function() {
+
+        log("Initialising map view");
+
+        this.tileSize = 30;
+        this.initialiseMap();
+
+    };
+
+    MapView.prototype.initialiseMap = function() {
+        this.jContainer
+                .css("width", this.tileSize * this.map.width)
+                .css("height", this.tileSize * this.map.height);
+        this.tileTemplate = jQuery("<div class='tile'></div>");
+    };
+
+    MapView.prototype.getTile = function(tileClass, x, y) {
+        return this.tileTemplate.clone()
+                .addClass(tileClass)
+                .css("left", x * this.tileSize)
+                .css("top", y * this.tileSize)
+    };
 
     MapView.prototype.updateAll = function() {
-        log("MapView is updating");
 
-        this.jContainer.empty();
+        var newContents = document.createDocumentFragment();
 
-        for (var x = 0; x < this.map.width; x++) {
-            for (var y = 0; y < this.map.height; y++) {
-                var tile =
-                        jQuery("<div></div>")
-                                .addClass("tile")
-                                .addClass(this.map.tileAt(x, y))
-                                .css({
-                            left: x * 30,
-                            top: y * 30
-                        });
-
-                this.jContainer.append(tile);
+        for (var y = 0; y < this.map.height; y++) {
+            for (var x = 0; x < this.map.width; x++) {
+                newContents.appendChild(
+                        this.getTile(this.map.tileAt(x, y), x, y)[0]
+                        );
             }
         }
+
+        this.jContainer.empty().append(newContents);
 
     };
 
