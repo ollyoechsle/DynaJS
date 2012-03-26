09e3b675e6bc555d7ac592cd619e36a8bd4c49f9
Olly Oechsle <owksley@gmail.com>
2012-03-26 21:11:44 +0100

    SFX

--- web/javascript/game/view/explosionview.js
+++ web/javascript/game/view/explosionview.js
@@ -23,7 +23,13 @@
                 [0]);
         }
         this.jContainer.append(fragment);
+       this.boom();
     };
+
+   ExplosionView.prototype.boom = function() {
+      var snd = new Audio("snd/explosion.wav");
+      snd.play();
+   };
 
     Dyna.ui.ExplosionView = ExplosionView;
 
