8be597ef2bfe115296e340a013206c05256fb68d
Olly Oechsle <owksley@gmail.com>
2012-04-15 09:17:51 +0100

    Added more gaussian functions to Math

--- web/javascript/prod/dyna-js-combined.js
+++ web/javascript/prod/dyna-js-combined.js
@@ -111,33 +111,67 @@ if (!Array.prototype.forEach) {
 
         return fBound;
     };
-}/**
- * Returns a Gaussian Random Number with mean 0.0 and std deviation 1.0;
- * @param {Number} mean The mean value, default 0.0
- * @param {Number} standardDeviation The standard deviation, default 1.0
- */
-Math.randomGaussian = function(mean, standardDeviation) {
+}(function() {
 
-    mean = mean || 0.0;
-    standardDeviation = isNaN(standardDeviation) ? 1.0 : standardDeviation;
+    /**
+     * Returns a Gaussian Random Number around a normal distribution defined by the mean
+     * and standard deviation parameters
+     *
+     * Uses the algorithm used in Java's random class, which in turn comes from
+     * Donald Knuth's implementation of the BoxÐMuller transform.
+     *
+     * @param {Number} [mean = 0.0] The mean value, default 0.0
+     * @param {Number} [standardDeviation = 1.0] The standard deviation, default 1.0
+     * @return {Number} A random number
+     */
+    Math.randomGaussian = function(mean, standardDeviation) {
 
-    if (this.hasAnotherGaussian) {
-        this.hasAnotherGaussian = false;
-        return (this.nextGaussian * standardDeviation) + mean;
-    } else {
-        var v1, v2, s, multiplier;
-        do {
-            v1 = 2 * Math.random() - 1; // between -1 and 1
-            v2 = 2 * Math.random() - 1; // between -1 and 1
-            s = v1 * v1 + v2 * v2;
-        } while (s >= 1 || s == 0);
-        multiplier = Math.sqrt(-2 * Math.log(s) / s);
-        this.nextGaussian = v2 * multiplier;
-        this.hasAnotherGaussian = true;
-        return (v1 * multiplier * standardDeviation) + mean;
+        mean = defaultTo(mean, 0.0);
+        standardDeviation = defaultTo(standardDeviation, 1.0);
+
+        if (Math.randomGaussian.nextGaussian !== undefined) {
+            var nextGaussian = Math.randomGaussian.nextGaussian;
+            delete Math.randomGaussian.nextGaussian;
+            return (nextGaussian * standardDeviation) + mean;
+        } else {
+            var v1, v2, s, multiplier;
+            do {
+                v1 = 2 * Math.random() - 1; // between -1 and 1
+                v2 = 2 * Math.random() - 1; // between -1 and 1
+                s = v1 * v1 + v2 * v2;
+            } while (s >= 1 || s == 0);
+            multiplier = Math.sqrt(-2 * Math.log(s) / s);
+            Math.randomGaussian.nextGaussian = v2 * multiplier;
+            return (v1 * multiplier * standardDeviation) + mean;
+        }
+
+    };
+
+    /**
+     * Returns a normal probability density function for the given parameters.
+     * The function will return the probability for given values of X
+     *
+     * @param {Number} [mean = 0] The center of the peak, usually at X = 0
+     * @param {Number} [standardDeviation = 1.0] The width / standard deviation of the peak
+     * @param {Number} [maxHeight = 1.0] The maximum height of the peak, usually 1
+     * @returns {Function} A function that will return the value of the distribution at given values of X
+     */
+    Math.getGaussianFunction = function(mean, standardDeviation, maxHeight) {
+
+        mean = defaultTo(mean, 0.0);
+        standardDeviation = defaultTo(standardDeviation, 1.0);
+        maxHeight = defaultTo(maxHeight, 1.0);
+
+        return function getNormal(x) {
+            return maxHeight * Math.pow(Math.E, -Math.pow(x - mean, 2) / (2 * (standardDeviation * standardDeviation)));
+        }
+    };
+
+    function defaultTo(value, defaultValue) {
+        return isNaN(value) ? defaultValue : value;
     }
 
-};Object.extend = function (SubClass, SuperClass) {
+})();Object.extend = function (SubClass, SuperClass) {
     function F() {}
 
     F.prototype = SuperClass.prototype;
